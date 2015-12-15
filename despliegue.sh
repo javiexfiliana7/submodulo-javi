92b8a1a0467c2aba91502a324a0fac53e7985b81
javiexfiliana7 <javiexfiliana7>
2015-12-15 18:00:57 +0100

    añadienco

--- despliegue.sh
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/javiexfiliana7/submodulo-javi.git
-cd submodulo-javi
-pwd
-heroku keys:add
-heroku git:clone -a combook
-heroku ps:scale web=1 --app combook
-heroku open --app combook
